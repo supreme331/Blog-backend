efadb32b63ecfd46c673c288a49786953079d471
Vasiliy Kabanov <supreme331@yandex.ru>
2022-08-20 13:38:36 +0300

    fix register validation

--- validations.js
+++ validations.js
@@ -8,7 +8,9 @@ export const registerValidation = [
     body('email', 'Неверный формат почты').isEmail(),
     body('password', 'Пароль должен быть минимум 5 символов').isLength({ min: 5 }),
     body('fullName', 'Укажите имя').isLength({ min: 3 }),
-    body('avatarUrl', 'Неверная ссылка на аватар').optional().isURL(),
+    body('avatarUrl', 'Неверная ссылка на аватар').optional().custom(value => {
+        return value === '' || body(value).isURL()
+    }),
 ]
 export const postCreateValidation = [
     body('title', 'Введите заголовок статьи').isLength({ min: 3 }).isString(),
